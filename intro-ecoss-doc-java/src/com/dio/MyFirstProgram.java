b12d97291776813dda0390814c8622b0041ce5ae
Matheus Alan <matheusalanpv@outlook.com>
2022-02-19 21:08:11 -0300

    01. Introdução ao ecossistema Java

---
+++ intro-ecoss-doc-java/src/com/dio/MyFirstProgram.java
@@ -0,0 +1,12 @@
+package com.dio;
+
+import com.dio.base.Order;
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        Order order = new Order("c123");
+        System.out.println(order);
+    }
+
+}
