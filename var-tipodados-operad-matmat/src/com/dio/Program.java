8505e4de7f9aedf336446aa74708c005b56e4f1e
Matheus Alan <matheusalanpv@outlook.com>
2022-02-21 16:27:56 -0300

    Iniciando curso Variáveis, Tipo de Dados e Operadores Matemáticos

---
+++ var-tipodados-operad-matmat/src/com/dio/Program.java
@@ -0,0 +1,46 @@
+package com.dio;
+
+public class Program {
+    public static void main(String[] args) {
+
+        int i;
+//        int i;
+        int I;
+//        int 1a;
+        int _1a; //não é uma boa pratica
+        int $aq; //não é uma boa pratica
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        final int j = 10; //não pode ser alterado
+//        j = 15;
+        int asrn2453md = 5;
+//        int asrn24 53md;
+        int asrn2$212_md = 10;
+//        int asrn2$21%2_md;
+
+        int quantidadeProduto = 50;
+        int QuantidadeProduto; //não é uma boa prática
+        final int NUMERO_TENTATIVAS = 5;
+        final int numeroTentativas = 5; //não é uma boa prática
+        int QUANTIDADE_OPCOES = 25; //não é uma boa prática
+        int qtdProd; //não é uma boa prática
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asrn2453md);
+        System.out.println(asrn2$212_md);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+    }
+}
