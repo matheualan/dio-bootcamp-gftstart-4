822cc88e04c5e859d02d3333160c5d83581307f0
Matheus Alan <matheusalanpv@outlook.com>
2022-02-24 16:54:19 -0300

    Exercício POO 01

--- programacao-orientada-objetos/src/com/dio/Program.java
+++ programacao-orientada-objetos/src/com/dio/Program.java
@@ -1,9 +1,16 @@
 package com.dio;
 
+import com.dio.classes.Carro;
+
 public class Program {
     public static void main(String[] args) {
 
+        Carro carro = new Carro("Preta", "Honda Civiv", 40);
 
+        carro.acelarar();
+        carro.frear();
 
+        carro.valorEnxerTanque(3.50);
+        
     }
 }
--- programacao-orientada-objetos/src/com/dio/classes/Carro.java
+++ programacao-orientada-objetos/src/com/dio/classes/Carro.java
@@ -6,4 +6,50 @@ public class Carro {
     private String modelo;
     private int tanqueL;
 
+    public Carro() {
+    }
+
+    public Carro(String cor, String modelo, int tanqueL) {
+        this.cor = cor;
+        this.modelo = modelo;
+        this.tanqueL = tanqueL;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getTanqueL() {
+        return tanqueL;
+    }
+
+    public void setTanqueL(int tanqueL) {
+        this.tanqueL = tanqueL;
+    }
+
+    public void acelarar() {
+        System.out.println("Acelerando carro.");
+    }
+
+    public void frear() {
+        System.out.println("Freando carro.");
+    }
+
+    public void valorEnxerTanque(double valorGasolina) {
+        double valorTotal = tanqueL * valorGasolina;
+        System.out.print("Valor total para encher tanque de " + tanqueL + "L: " + valorTotal);
+    }
+
 }
