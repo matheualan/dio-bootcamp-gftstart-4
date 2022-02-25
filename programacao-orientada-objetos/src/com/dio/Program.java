ae1ad1a98e25771c71549570b5333e3f81586511
Matheus Alan <matheusalanpv@outlook.com>
2022-02-24 21:00:35 -0300

    Exercicio upcast/downcast

--- programacao-orientada-objetos/src/com/dio/Program.java
+++ programacao-orientada-objetos/src/com/dio/Program.java
@@ -1,10 +1,14 @@
 package com.dio;
 
+import com.dio.classes.Caminhao;
 import com.dio.classes.Carro;
+import com.dio.classes.Moto;
+import com.dio.classes.Veiculo;
 
 public class Program {
     public static void main(String[] args) {
 
+//        EXERCÍCIO 01
         Carro carro1 = new Carro("Preta", "Honda Civiv", 40);
         Carro carro2 = new Carro("Prata", "Jetta", 60);
         Carro carro3 = new Carro("Vermelha", "Captiva", 120);
@@ -25,6 +29,16 @@ public class Program {
         System.out.println("Modelo: " + carro3.getModelo());
         System.out.println("Cor: " + carro3.getCor());
         carro3.valorEnxerTanque(3.50);
+
+//        EXERCÍCIO 02
+        Carro car = (Carro) new Veiculo();
+        Veiculo veic = new Carro();
+
+        Moto moto = (Moto) new Veiculo();
+        Veiculo veicu = new Moto();
+
+        Caminhao truck = (Caminhao) new Veiculo();
+        Veiculo veiculo = new Caminhao();
 
     }
 }
